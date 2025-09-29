1e03d56e8ce69f3f35997e79a648763100f6cea2
PrabathVIJA <prabath.kool@gmail.com>
2025-09-29 10:36:48 +0530

    first commit

---
+++ tests/homeTest.spec.ts
@@ -0,0 +1,6 @@
+import { test } from "@playwright/test";
+
+test("go there", async ({ page }) => {
+  await page.goto("https://askomdch.com/");
+  await page.getByText("Store").first().click();
+});
