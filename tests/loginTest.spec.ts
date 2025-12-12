e0707b9b4de8a815d23cb62fc11aabefded5484d
PrabathVIJA <prabath.kool@gmail.com>
2025-12-12 16:07:04 +0530

    implemented storage and cookies

---
+++ tests/loginTest.spec.ts
@@ -0,0 +1,18 @@
+import { test, expect } from "@playwright/test";
+import { PageManager } from "../managers/PageManager";
+import { AccountPageObject } from "../page-objects/AccountPageObject";
+
+test.describe("Login test suite", async () => {
+  test.use({ storageState: "auth.json" });
+  let pageManager: PageManager;
+  let account: AccountPageObject;
+  test.beforeEach("Login", async ({ page }) => {
+    pageManager = new PageManager(page);
+    account = pageManager.accountPage();
+    await page.goto("https://askomdch.com/account/");
+  });
+  test("validate home Page url", async ({ page }) => {
+    const text = await account.getHelloText();
+    console.log(text);
+  });
+});
