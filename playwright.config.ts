11212658ec9f4e260b0bc7010735c7d08eb4a07e
PrabathVIJA <prabath.kool@gmail.com>
2026-01-01 14:46:46 +0530

    changed view port

--- playwright.config.ts
+++ playwright.config.ts
@@ -14,13 +14,13 @@ import { defineConfig, devices } from "@playwright/test";
 export default defineConfig({
   testDir: "./tests",
   /* Run tests in files in parallel */
-  fullyParallel: false,
+  fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 4 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ["list"], // keeps console output
@@ -29,9 +29,10 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
+    viewport: { width: 1920, height: 1080 },
     baseURL: "https://askomdch.com/",
     screenshot: "only-on-failure",
-
+    video: "retain-on-failure",
     trace: "retain-on-failure",
   },
 
@@ -43,10 +44,6 @@ export default defineConfig({
         browserName: "chromium",
         channel: "chrome", // ensures real Chrome, not just Chromium
         headless: !!process.env.CI,
-        viewport: null, // let the window decide size
-        launchOptions: {
-          args: ["--start-maximized"], // start browser maximized
-        },
       },
     },
 
